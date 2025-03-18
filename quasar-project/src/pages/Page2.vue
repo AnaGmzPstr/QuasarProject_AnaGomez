5adb3621f3223775444f9e0b939d46a3e20f78a6
AnaGmzPastr <ana.gomez.7e6@itb.cat>
2025-03-18 20:01:43 +0100

    pagina 2 hecha

--- quasar-project/src/pages/Page2.vue
+++ quasar-project/src/pages/Page2.vue
@@ -1,11 +1,38 @@
+<script setup>
+    import { ref, onMounted } from 'vue'
+    import axios from 'axios';
+
+    const users = ref([]);
+    const loading = ref(false);
+    const error = ref(null);
+
+    const fetchData = async () => {
+        try{
+            const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+            users.value = response.data
+        } catch (error) {
+            error.value ='Error'
+        } finally {
+            loading.value = false;
+        }
+    }
+    onMounted(fetchData);
+</script>
+
 <template>
     <q-page>
       <h1>Página 2</h1>
-      <p>Bienvenido a la Página 2</p>
+
+      <div v-if="loading">Cargando datos...</div>
+      <div v-else-if="error">{{ error }}</div>
+      <div v-else>
+        <h2>Usuarios:</h2>
+        <ul>
+            <li v-for="user in users" :key="user.id">
+                {{ user.name }} - {{ user.email }}
+            </li>
+        </ul>
+      </div>
     </q-page>
   </template>
-  
-  <script setup>
-    // Puedes agregar lógica si la necesitas
-  </script>
   
